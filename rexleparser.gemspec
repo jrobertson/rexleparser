3c3b2552871bf043deecc371507925a007f50fc4
James Robertson <james@r0bertson.co.uk>
2010-11-09 12:45:33 +0000

    now strips out the processing instruction

--- rexleparser.gemspec
+++ rexleparser.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'rexleparser'
-  s.version = '0.1.0'
+  s.version = '0.1.1'
   s.summary = 'rexleparser'
   s.files = Dir['lib/**/*.rb']
 end
---
+++ test_rexleparser.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/ruby
+
+# file: test_rexleparser.rb
+ 
+require 'testdata'
+#require 'rexleparser'
+require '/home/james/learning/ruby/rexleparser'
+
+testdata = Testdata.new('testdata_rexleparser.xml')
+
+testdata.paths do |path|
+
+  testdata.find_by('XML validation').each do |title|
+
+    path.tested? title do |input, output|
+      result = input.data('xml') {|xml| RexleParser.new(xml).to_a.inspect}
+      puts result
+      puts
+      expected = output.data('xml')
+      result == expected
+    end
+  end
+
+end
+
+puts testdata.passed?
+puts testdata.score
+puts testdata.instance_variable_get(:@success).map.with_index.select{|x,i| x == false}.map(&:last)
+
+
