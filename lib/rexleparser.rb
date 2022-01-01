020fe2d980376253f14e43a8fa9ffe232a20e7bc
James Robertson <james@r0bertson.co.uk>
2022-01-01 17:59:57 +0000

    bug fix: XML attributes are no longer automatically unescaped when used with the Rexle gem to display XML.

--- lib/rexleparser.rb
+++ lib/rexleparser.rb
@@ -7,49 +7,49 @@
 class Attributes < Hash
 
   class Value < String
-    
+
     def initialize(value)
       #jr2020-04-30 super(value.gsub("'", '&apos;'))
       super(value)
     end
-          
+
     def <(val2)
       self.to_f < val2.to_f
-    end      
-    
+    end
+
     def >(val2)
       self.to_f > val2.to_f
     end
-    
+
     def inspect()
       super().gsub('&lt;','<',).gsub('&gt;','>').gsub('&pos;',"'")
     end
-    
-    def to_s()
+
+    def to_s(unescape: true)
       #jr2020-07-18 self.gsub('&lt;','<',).gsub('&gt;','>').gsub('&pos;',"'")
-      self.gsub('&amp;','&').gsub('&pos;',"'")
+      self.gsub('&amp;','&').gsub('&pos;',"'") if unescape
     end
-    
-  end  
-  
+
+  end
+
   def initialize(h={})
     super().merge! h
   end
-  
+
   def []=(k,v)
     super(k, k != :class ? Value.new(v) : v)
   end
-  
+
   def merge(h)
 
-    h2 = h.inject({}) do |r, kv| 
+    h2 = h.inject({}) do |r, kv|
       k, raw_v = kv
       v = raw_v.is_a?(String) ? Value.new(raw_v) : raw_v
-      r.merge(k => v) 
+      r.merge(k => v)
     end
-    
+
     super(h2)
-    
+
   end
 end
 
@@ -65,7 +65,7 @@ class RexleParser
     super()
     s = raw_s.clone.strip
     return if s.empty?
-    
+
     raw_xml, raw_instrctns = if s.lines.first =~ /<?xml/ then
       s.split(/(?=\?>\s*<\w)/,2).reverse
     else
@@ -77,10 +77,10 @@ class RexleParser
     @to_a = reverse(parse_node(raw_xml.strip.reverse))
 
   end
-    
+
   private
 
-  
+
   def scan_next(r, tagname)
 
     j = tagname
@@ -89,7 +89,7 @@ class RexleParser
 
       # end tag match
       tag = r[/^>[^<]+</]
-      
+
       if tag[1][/[ \w"']/] and  tag[-2] != '/'  then
 
         # is it the end tag to match the start tag?
@@ -97,18 +97,18 @@ class RexleParser
         end_tag = tag[/^>[^>]*#{j}<$/]
 
         if end_tag then
-          
+
           j = nil
           return   [:end_tag, end_tag]
 
         elsif tag[/^>[^>]*\w+<$/] then
           # broken tag found
           broken_tag = tag
-          return [:child, [nil, [], broken_tag]] if broken_tag          
+          return [:child, [nil, [], broken_tag]] if broken_tag
         else
-          
+
           text, newtag =  tag.sub('>',';tg&').split(/>/,2)
-          
+
           if newtag then
             tag = newtag
             r.prepend '>' + tag
@@ -117,16 +117,16 @@ class RexleParser
           return [:child, text]
         end
       elsif r[0,3] == '>--' then   # comment tag found
-          
+
         r.slice!(0,3)
         i = r =~ /(\-\-!<)/
         s = r.slice!(0,i)
         r.slice!(0,4)
 
         tagname, content = ['-!',s]
-      
+
         return [:child, [">#{tagname}<", [content], ">#{tagname}/<"]]
-        
+
       elsif r[0,3] == '>]]' then   # CDATA tag found
 
         r.slice!(0,3)
@@ -136,22 +136,22 @@ class RexleParser
 
         tagname, content = ['[!',s]
 
-        return [:child, [">#{tagname}<", [content], ">#{tagname}/<"]]        
-        
+        return [:child, [">#{tagname}<", [content], ">#{tagname}/<"]]
+
       elsif tag[/>\/|\/<$/] or tag[/^>.*[\w!]+\/<$/] then
-                
-        return [:newnode]        
-        
+
+        return [:newnode]
+
       else
-  
-        r.sub!('>',';tg&')      
+
+        r.sub!('>',';tg&')
         i = r =~ />(?:[\-\/"'\w]|\]\])/ # collect until a tag is found or a CDATA element
         text = r.slice!(0,i)
 
         return [:child, text] if text
 
       end # end of tag match
-      
+
     else
 
       # it's a text value
@@ -161,12 +161,12 @@ class RexleParser
       return [:child, text] if text
     end
   end
-  
+
   def parse_node(r, j=nil)
-    
+
     return unless r.length > 0
     tag = r.slice!(/^>[^<]+</) if (r =~ /^>[^<]+</) == 0
-    tagname = tag[/([\w!:]+)\/?<$/,1] 
+    tagname = tag[/([\w!:]+)\/?<$/,1]
 
     # self closing tag?
     if tag[/^>\/.*#{tagname}<$/m] then
@@ -180,16 +180,16 @@ class RexleParser
                 start_tag.reverse + '; context: ' + r[0..120].reverse.inspect
     end
 
-    until end_tag do 
-      
-      key, res = scan_next r, tagname      
-      
-      case key 
+    until end_tag do
+
+      key, res = scan_next r, tagname
+
+      case key
       when :end_tag
         end_tag = res
         r2 = [start_tag, children, end_tag]
         end_tag = nil
-        
+
         return r2
       when :child
         children << res
@@ -204,13 +204,13 @@ class RexleParser
   end
 
   def get_attributes(raw_attributes)
-    
+
     r1 = /([\w\-:\(\)]+\='[^']*)'/
     r2 = /([\w\-:\(\)]+\="[^"]*)"/
-    
+
     r =  raw_attributes.scan(/#{r1}|#{r2}/).map(&:compact)\
                                   .flatten.inject(Attributes.new) do |r, x|
-      attr_name, raw_val = x.split(/=/,2) 
+      attr_name, raw_val = x.split(/=/,2)
       val = attr_name != 'class' ? raw_val[1..-1] : raw_val[1..-1].split
       r.merge(attr_name.to_sym => val)
     end
@@ -219,17 +219,17 @@ class RexleParser
   end
 
   def reverse(raw_obj)
-    
+
     return unless raw_obj
     obj = raw_obj.clone
     return obj.reverse! if obj.is_a? String
 
     tag = obj.pop.reverse
-    
+
     children = obj[-1]
 
     r = children.reverse.map {|x| reverse(x)}
-    
+
     return [tag[/[!\-\w:\[]+/], get_attributes(tag), *r]
-  end  
+  end
 end
--- rexleparser.gemspec
+++ rexleparser.gemspec
@@ -1,13 +1,13 @@
 Gem::Specification.new do |s|
   s.name = 'rexleparser'
-  s.version = '0.9.7'
+  s.version = '0.9.8'
   s.summary = 'Rexleparser is an XML parser used by the Rexle gem'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rexleparser.rb'] 
   s.signing_key = '../privatekeys/rexleparser.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/rexleparser'
   s.required_ruby_version = '>= 2.1.2'
 end
