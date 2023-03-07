d88bb35ea8109f0686514af2d262e1538501b670
James Robertson <james@r0bertson.co.uk>
2023-03-07 12:56:29 +0000

    bug fix: Reverted to the previous version of the code until the new code is thoroughly tested.

--- lib/rexleparser.rb
+++ lib/rexleparser.rb
@@ -62,60 +62,154 @@ class Attributes < Hash
   end
 end
 
-
 class RexleParserException < Exception
 end
 
 class RexleParser
 
-  attr_reader :stack
+  attr_reader :instructions, :doctype, :to_a
 
-  def initialize(raws, debug: false)
+  def initialize(raw_s)
 
-    s = raws.strip
-    @debug = debug
-    @a = []
-    @stack = []
+    super()
+    s = raw_s.clone.strip
+    return if s.empty?
     
     raw_xml, raw_instrctns = if s.lines.first =~ /<?xml/ then
-      s.split(/(?<=\?>)/,2).reverse
+      s.split(/(?=\?>\s*<\w)/,2).reverse
     else
       s
     end
-    puts 'raw_xml: ' + raw_xml.inspect if @debug
     @instructions = raw_instrctns ? \
                               raw_instrctns.scan(/<\?([\w-]+) ([^\?]+)/) : []
     @doctype = s.slice!(/<!DOCTYPE html>\n?/) if s.lines.first =~ /<\!DOCTYPE/
-        
-    # scancom is run twice because we 1st check for comment tags and then cdata tags
-    @a = parse(scancom(scancom(raw_xml), type: :cdata)).flatten(1)
+    @to_a = reverse(parse_node(raw_xml.strip.reverse))
 
   end
-
-  def to_a()
-    @a
-  end
-
-  private
-  
-  def ehead(raws)
-
-    s = raws.lstrip
-    puts '_s: ' + s.inspect if @debug
-    # fetch the element head
-    tag = s =~ /<[^>]+\/?>/
-    s2 = s[tag+1..-1]
-    tagb = s2 =~ />/
-    return unless tag
-
-    len = tagb+1-tag
     
-    if @debug then
-      puts 'ehead()/detail: ' + [tag, tagb, len, s[tag,len+1]].inspect
+  private
+
+  
+  def scan_next(r, tagname)
+
+    j = tagname
+
+    if r[0] == '>' then
+
+      # end tag match
+      tag = r[/^>[^<]+</]
+      
+      if tag[1][/[ \w"']/] and  tag[-2] != '/'  then
+
+        # is it the end tag to match the start tag?
+        tag = r.slice!(/^>[^<]+</)
+        end_tag = tag[/^>[^>]*#{j}<$/]
+
+        if end_tag then
+          
+          j = nil
+          return   [:end_tag, end_tag]
+
+        elsif tag[/^>[^>]*\w+<$/] then
+          # broken tag found
+          broken_tag = tag
+          return [:child, [nil, [], broken_tag]] if broken_tag          
+        else
+          
+          text, newtag =  tag.sub('>',';tg&').split(/>/,2)
+          
+          if newtag then
+            tag = newtag
+            r.prepend '>' + tag
+          end
+
+          return [:child, text]
+        end
+      elsif r[0,3] == '>--' then   # comment tag found
+          
+        r.slice!(0,3)
+        i = r =~ /(\-\-!<)/
+        s = r.slice!(0,i)
+        r.slice!(0,4)
+
+        tagname, content = ['-!',s]
+      
+        return [:child, [">#{tagname}<", [content], ">#{tagname}/<"]]
+        
+      elsif r[0,3] == '>]]' then   # CDATA tag found
+
+        r.slice!(0,3)
+        i = r =~ /(\[ATADC\[!<)/
+        s = r.slice!(0,i)
+        r.slice!(0,9)
+
+        tagname, content = ['[!',s]
+
+        return [:child, [">#{tagname}<", [content], ">#{tagname}/<"]]        
+        
+      elsif tag[/>\/|\/<$/] or tag[/^>.*[\w!]+\/<$/] then
+                
+        return [:newnode]        
+        
+      else
+  
+        r.sub!('>',';tg&')      
+        i = r =~ />(?:[\-\/"'\w]|\]\])/ # collect until a tag is found or a CDATA element
+        text = r.slice!(0,i)
+
+        return [:child, text] if text
+
+      end # end of tag match
+      
+    else
+
+      # it's a text value
+      i = r =~ />(?:[\-\/"'\w]|\]\])/ # collect until a tag is found or a CDATA element
+      text = r.slice!(0,i)
+
+      return [:child, text] if text
+    end
+  end
+  
+  def parse_node(r, j=nil)
+    
+    return unless r.length > 0
+    tag = r.slice!(/^>[^<]+</) if (r =~ /^>[^<]+</) == 0
+    tagname = tag[/([\w!:]+)\/?<$/,1] 
+
+    # self closing tag?
+    if tag[/^>\/.*#{tagname}<$/m] then
+      return [">/#{tagname}<", [], "#{tag.sub(/>\//,'>')}"]
     end
 
-    [s[tag,len+1], s[len+1..-1]]
+    start_tag, children, end_tag = tag, [], nil
 
+    unless start_tag[1..-3][/\w+$/] then
+      raise RexleParserException, 'invalid closing tag found ' + \
+                start_tag.reverse + '; context: ' + r[0..120].reverse.inspect
+    end
+
+    until end_tag do 
+      
+      key, res = scan_next r, tagname      
+      
+      case key 
+      when :end_tag
+        end_tag = res
+        r2 = [start_tag, children, end_tag]
+        end_tag = nil
+        
+        return r2
+      when :child
+        children << res
+      when :newnode
+        children << parse_node(r, tagname)
+      else
+        break
+      end
+    end
+
+    [start_tag,  children, end_tag]
   end
 
   def get_attributes(raw_attributes)
@@ -133,151 +227,18 @@ class RexleParser
     return r
   end
 
-  def parse(raws, a=[], cur=nil)
-
-    s = raws #.lstrip
+  def reverse(raw_obj)
     
-    if @debug then
-      puts '.parse() s: ' + s.inspect[0..600]
-      puts '.parse() a: ' + a.inspect[0..699]
-      puts '.parse() cur: ' + cur.inspect[0..799]
-    end
+    return unless raw_obj
+    obj = raw_obj.clone
+    return obj.reverse! if obj.is_a? String
 
-    # if self-closing tag
-    if s =~ /^<[^<]+\/>/ then
-
-      tag = s[/^<[^<]+\/>/]
-      puts 'parse() self-closing/tag: ' + tag.inspect  if @debug
-      tail = $'
-      
-      if @debug then
-        puts 'parse() self-closing tag found'
-        puts 'parse()/tail: ' + tail.inspect
-      end
-      
-      a2 = parsetag(tag)
-      puts '_a: ' + a.inspect if @debug
-      cur ? a.last << a2 : a << a2
-      
-      parse(tail, a, cur)
-
-    # is it the head?
-    elsif (s =~ /^<[^\/>]+>/) == 0 then
-      
-      puts 'parse()/head found' if @debug
-
-      tag, tail = ehead(s)
-      
-      if @debug then
-        puts 'parse() tag: ' + tag.inspect
-        puts 'parse() tail: ' + tail.inspect
-      end
-      # add it to the list
-      a2 = parsetag(tag)
-
-      puts '_cur: ' + cur.inspect  if @debug
-      if cur then
-        cur << a2
-        cur2 = cur.last
-      else
-        a << a2
-        cur2 = a.last
-      end
-
-      puts '_a: ' + a.inspect  if @debug
-
-      # add it to the stack
-      @stack.push cur2
-
-      parse(tail, a, cur2)
-      
-    elsif (s =~ /^[^<]/) == 0
-
-      puts 'parse() we have text!'  if @debug
-      text = raws[/[^<]+/m] #
-      remaining = $'
-      
-      if @debug then
-        puts 'parse() text: ' + text.inspect
-        puts 'cur tag: ' + cur[0].inspect
-      end
-      
-      cur << if cur[0][0] == '!' then
-        text.gsub('&lt;','<').gsub('&gt;','>').gsub('&amp;','&')
-      else
-        text.gsub(/>/,'&gt;').gsub(/</, '&lt;')
-      end
-
-      puts 'remaining: ' + remaining.inspect  if @debug
-      parse(remaining, a, cur) if remaining.length > 0
-            
-      
-    # is it a closing tag?
-    elsif s =~ /^\s?<\/\w+>/m
-
-      tail = s[/^\s*<\/\w+>(.*)/m,1]
-      
-      if @debug then
-        puts 'parse()/closing tag ' + s[/^\s*<\/\w+>/].inspect
-        puts '>a: ' + a.inspect
-      end
-      
-      @stack.pop
-      #a << []
-      parse(tail, a, @stack.last)
-        
-    elsif s.empty? and @stack.length > 0
-      
-      puts 'parse() no end tag!'  if @debug
-
-    end
-
-    return a
-
-  end
-
-  # we parse the tag because it contains more than just the name it often contains attributes
-  #
-  def parsetag(s)
+    tag = obj.pop.reverse
     
-    puts 'parsetag:' + s.inspect  if @debug
-    rawtagname, rawattr = s[1..-2].sub(/\/$/,'').match(/^(\w+) *(.*)/)\
-      .values_at(1,2)
+    children = obj[-1]
 
-    tagname = case rawtagname.to_sym
-    when :_comment
-      '!-'
-    when :_cdata
-      '!['
-    else
-      rawtagname
-    end
-
-    [tagname, get_attributes(rawattr)]
-  end
-
-  def scancom(s, type=:comment)
-
-    tag1 = ['<!--', '-->', 'comment', '<!--']
-    tag2 = ['<![CDATA[', '\]\]>', 'cdata', '\<!\[CDATA\[']
-    tag = type == :comment ? tag1 : tag2
-
-    #puts 'tag: ' + tag.inspect
-    istart = s =~ /#{tag[3]}/
-    return s unless istart
-
-    iend = s =~ /#{tag[1]}/
-    comment ="<_%s>%s</_%s>" % [tag[2], s[istart+tag[0].length.. iend-1].gsub('&','&amp;').gsub('<','&lt;').gsub('>','&gt;'), tag[2]]
-
-    if @debug then
-      puts 'comment: ' + comment.inspect
-      # construct the new string
-      puts 'istart: ' + istart.inspect
-    end
-                                              
-    s3 = s[0,istart].to_s + comment + s[iend+3..-1]
-    scancom(s3, type)
-
-  end
-
+    r = children.reverse.map {|x| reverse(x)}
+    
+    return [tag[/[!\-\w:\[]+/], get_attributes(tag), *r]
+  end  
 end
--- rexleparser.gemspec
+++ rexleparser.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name = 'rexleparser'
-  s.version = '1.0.0'
+  s.version = '1.0.1'
   s.summary = 'Rexleparser is an XML parser used by the Rexle gem'
   s.authors = ['James Robertson']
   s.files = Dir['lib/rexleparser.rb'] 
